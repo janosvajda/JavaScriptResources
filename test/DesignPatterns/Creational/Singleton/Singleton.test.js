10f728af1c4206db17efaecf678c09aeb3a0acad
Janos Vajda <vajdajanos@gmail.com>
2020-12-13 09:01:19 +0000

    Singleton class loading in its test

--- test/DesignPatterns/Creational/Singleton/Singleton.test.js
+++ test/DesignPatterns/Creational/Singleton/Singleton.test.js
@@ -1,4 +1,8 @@
 var assert = require('assert');
+
+const Singleton = require('../../../../DesignPatterns/Creational/Singleton/Singleton.js');
+
+
 describe('Singleton class', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
