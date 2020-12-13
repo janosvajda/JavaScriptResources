626833f738a1ce1eab410c995ee0c5f773acfbaa
Janos Vajda <vajdajanos@gmail.com>
2020-12-13 09:05:48 +0000

    Code formatting of Singleton test

--- test/DesignPatterns/Creational/Singleton/Singleton.test.js
+++ test/DesignPatterns/Creational/Singleton/Singleton.test.js
@@ -3,10 +3,9 @@ var assert = require('assert');
 const SingletonInstance1 = require('../../../../DesignPatterns/Creational/Singleton/Singleton.js');
 const SingletonInstance2 = require('../../../../DesignPatterns/Creational/Singleton/Singleton.js');
 
-
-describe('Singleton class', function() {
-  describe('#Equality of two Singleton instances()', function() {
-    it('SingletonInstance1 and SingletonInstance2 must be equal', function() {
+describe('Singleton class', function () {
+  describe('#Equality of two Singleton instances()', function () {
+    it('SingletonInstance1 and SingletonInstance2 must be equal', function () {
       assert.equal(SingletonInstance1, SingletonInstance2);
     });
   });
