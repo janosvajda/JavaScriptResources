db895f9d1bec4fdc03461174341626875252c918
Janos Vajda <vajdajanos@gmail.com>
2020-12-15 07:18:29 +0000

    Demeter of law example and its test

--- Principles/LawOfDemeter/LawOfDemeter.js
+++ Principles/LawOfDemeter/LawOfDemeter.js
@@ -39,24 +39,20 @@ class C {
 }
 
 /**
- * Break Demeter of law example.
+ * Demeter of law example.
  *
  * Class D needs to get values from the class A.
- * In this case, we break the Demeter of law as we can retrieve in this way only:
+ * In this case, we do not break the Demeter of law as we can retrieve in this way only:
  * const objD = new classD('Hello');
- * console.log("Value of class A: ", objD.getC().getB().getA().getValue());
- *
- * You can find the fix in the file: LawOfDemeter.js
- *
- * Unit tests still will pass, of course as it works. But it needs to be change.
+ * console.log("Value of class A: ", objD.getAValue());
  */
 class D {
   constructor() {
-    this.C = new C('Hello from C.');
+    this.A = new A('Hello from A.');
   }
 
-  getC() {
-    return this.C;
+  getAValue(postalCode) {
+    return this.A.getValue();
   }
 }
 
--- test/Principles/LawOfDemeter/LawOfDemeter.test.js
+++ test/Principles/LawOfDemeter/LawOfDemeter.test.js
@@ -1,13 +1,13 @@
 var assert = require('assert');
 
-const classD = require('../../../Principles/LawOfDemeter/BreakLawOfDemeter.js');
+const classD = require('../../../Principles/LawOfDemeter/LawOfDemeter.js');
 
 const objD = new classD();
 
 describe('Test of break of Demeter of law', function () {
   describe('#Equality of two Class A value', function () {
     it('Class A value must be "Hello from A."', function () {
-      assert.equal(objD.getC().getB().getA().getValue(), 'Hello from A.');
+      assert.equal(objD.getAValue(), 'Hello from A.');
     });
   });
 });
